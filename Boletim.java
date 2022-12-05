3bfc0bbb1d02c954ce22d75770e884a9ad5475bd
Klaus von Brand <luaboldt@gmail.com>
2022-12-05 20:21:33 -0300

    Add files via upload

--- Boletim.java
+++ Boletim.java
@@ -3,10 +3,22 @@ package projeto.pkgfinal.pw2;
 public class Boletim {
     private int codigo;
     private int semestre;
-    private float notas;
-    private char resultado;
-    private char disciplina;
+    private float notas[];
+    private float media;
+    private boolean resultado;
 
+
+        
+    Boletim(int codigo, int semestre, float notas[], float media, boolean resultado){
+        this.codigo = codigo;
+        this.semestre = semestre;
+        for(int i = 0; i < notas.length; i++){
+            this.notas[i] = notas[i];
+        }
+        this.media = media;
+        this.resultado = resultado;
+    }
+    
     public int getSemestre() {
         return semestre;
     }
