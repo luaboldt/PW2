86ed98a4dece31cf500fa40c6add3118669635e1
marck0101 <marcos.henrique.a7x@gmail.com>
2022-12-12 16:24:23 -0300

    DAO, model, controllers

---
+++ Projeto Final PW2/src/models/Boletim.java
@@ -0,0 +1,57 @@
+package models;
+
+import projeto.pkgfinal.pw2.*;
+
+public class Boletim {
+
+    private int codigo;
+    private int semestre;
+    private float notas[];
+    private float media;
+    private boolean resultado;
+    private float disciplina;
+
+    Boletim(
+            int codigo,
+            int semestre,
+            float notas[],
+            float media,
+            boolean resultado
+    ) {
+        this.codigo = codigo;
+        this.semestre = semestre;
+        for (int i = 0; i < notas.length; i++) {
+            this.notas[i] = notas[i];
+        }
+        this.media = media;
+        this.resultado = resultado;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+//    public float getNotas() {;;
+//        return notas;
+//    }
+//    public void setNotas(float notas) {;
+//        this.notas = notas;
+//    }
+//    public char getResultado() {
+//        return resultado;
+//    }
+//    public void setResultado(char resultado) {
+//        this.resultado = resultado;
+//    }
+//    public char getDisciplina() {
+//        return disciplina;
+//    }
+    public void setDisciplina(char disciplina) {
+        this.disciplina = disciplina;
+    }
+
+}
---
+++ Projeto Final PW2/src/models/Professor.java
@@ -0,0 +1,47 @@
+package models;
+
+import projeto.pkgfinal.pw2.*;
+import java.util.Scanner;
+
+public class Professor extends Pessoa {
+
+    private String formacao;
+
+    Professor(
+            String nome,
+            String cpf,
+            String rg,
+            String telefone,
+            String endereco,
+            int idade,
+            String formacao
+    ) {
+        super(nome, cpf, rg, telefone, endereco, idade);
+        this.formacao = formacao;
+    }
+
+    Professor() {
+        super();
+
+        Scanner input = new Scanner(System.in);
+        String vFormacao;
+        int vCodigo;
+
+        System.out.printf("Informe seu codigo: ");
+        vCodigo = input.nextInt();
+        System.out.printf("Informe sua formacao: ");
+        vFormacao = input.next();
+
+        this.formacao = vFormacao;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+
+    }
+
+}
